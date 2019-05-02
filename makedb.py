5893c422c641c9343e33bb3fbe3c305f769274f4
Bowie Smith <r.bowie.smith@gmail.com>
2019-05-02 09:24:58 -0400

    Add foreign key ref to rental_agreement table

--- makedb.py
+++ makedb.py
@@ -4,12 +4,14 @@ conn = sqlite3.connect('soap.db')
 if conn is not None:
 	c = conn.cursor()
 
-	sql_cr_gsa_office = """CREATE TABLE IF NOT EXISTS gsa_office (
+	sql_cr_gsa_office = """
+                CREATE TABLE IF NOT EXISTS gsa_office (
 		office_name text PRIMARY KEY,
 		office_city text,
 		sqft_managed real);"""
 
-	sql_cr_customer_agency = """CREATE TABLE IF NOT EXISTS customer_agency ( 
+	sql_cr_customer_agency = """
+                CREATE TABLE IF NOT EXISTS customer_agency ( 
 		agency_id text PRIMARY KEY,
 		street_name text,
 		street_number text,
@@ -18,15 +20,18 @@ if conn is not None:
 		zip_code text,
 		phone_number text);"""
 
-	sql_cr_rental_agreements = """CREATE TABLE IF NOT EXISTS rental_agreements (
+	sql_cr_rental_agreements = """
+                CREATE TABLE IF NOT EXISTS rental_agreements (
 		ra_id text PRIMARY KEY,
 		office_name text,
 		rent_amount real, 
 		end_month integer,
 		end_day integer,
-		end_year integer );"""
+		end_year integer,
+                FOREIGN KEY (office_name) REFERENCES gsa_office(office_name));"""
 
-	sql_cr_agency_rental_agreements = """CREATE TABLE IF NOT EXISTS agency_rental_agreements (
+	sql_cr_agency_rental_agreements = """
+                CREATE TABLE IF NOT EXISTS agency_rental_agreements (
 		agency_id text,
 		ra_id text,
 		FOREIGN KEY (agency_id) REFERENCES customer_agency(agency_id),
